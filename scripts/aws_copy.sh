08eaab49805d6c0bcae355b60e19c5ee68202b7b
Owais <awaisajaz1@gmail.com>
2026-04-07 15:42:14 +0500

    Describe your change

--- scripts/aws_copy.sh
+++ scripts/aws_copy.sh
@@ -1,2 +1,42 @@
-aws s3 cp CAVideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=ca/
+# aws s3 cp CAVideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=ca/
+# aws s3 cp CA_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=ca/
+
+# Canada
+aws s3 cp CAvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=ca/
 aws s3 cp CA_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=ca/
+
+# Germany
+aws s3 cp DEvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=de/
+aws s3 cp DE_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=de/
+
+# France
+aws s3 cp FRvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=fr/
+aws s3 cp FR_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=fr/
+
+# Great Britain
+aws s3 cp GBvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=gb/
+aws s3 cp GB_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=gb/
+
+# India
+aws s3 cp INvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=in/
+aws s3 cp IN_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=in/
+
+# Japan
+aws s3 cp JPvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=jp/
+aws s3 cp JP_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=jp/
+
+# Korea
+aws s3 cp KRvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=kr/
+aws s3 cp KR_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=kr/
+
+# Mexico
+aws s3 cp MXvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=mx/
+aws s3 cp MX_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=mx/
+
+# Russia
+aws s3 cp RUvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=ru/
+aws s3 cp RU_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=ru/
+
+# United States
+aws s3 cp USvideos.csv s3://bronze-youtube-analytics-2026/youtube/raw/region=us/
+aws s3 cp US_category_id.json s3://bronze-youtube-analytics-2026/youtube/raw_lookups/region=us/
